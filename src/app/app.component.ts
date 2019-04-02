427f080e8eb4876e2ad4a3bbf8f42c21d07803a1
shashanktester <shashusushu0827@gmail.com>
2019-04-02 19:06:22 +0530

    firs commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, TemplateRef  } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+}
---
+++ src/app/employees/list-employees.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../models/employee.model';
+
+
+@Component({
+  selector: 'app-list-employees',
+  templateUrl: './list-employees.component.html',
+  styleUrls: ['./list-employees.component.css']
+})
+export class ListEmployeesComponent implements OnInit {
+  employees: Employee[] = [{
+    id: 1,
+    name: "Soumya",
+    gender: "Male",
+    email: "soumya@gmail.com",
+    contactPreference: "Email",
+    dateOfBirth: new Date("04/23/1992"),
+    department: "IT",
+    isActive: true,
+    photoPath: "assets/images/john.png"
+  },
+
+  {
+
+    id: 2,
+    name: "Shashank",
+    gender: "Male",  
+    phoneNumber: 99999999,
+    contactPreference: "phone",
+    dateOfBirth: new Date("02/27/1990"),
+    department: "Finance",
+    isActive: true,
+    photoPath: "assets/images/mark.png"
+  },
+
+  {
+
+    id: 3,
+    name: "Mary",
+    gender: "Female",
+    email: "Mary@gmail.com",
+    contactPreference: "email",
+    dateOfBirth: new Date('05/25/1988'),
+    department: "HR",
+    isActive: false,
+    photoPath: "assets/images/mary.png"    
+  }]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
